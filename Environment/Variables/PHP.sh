2b17f692836a376b43928b0a8fc531f743348014
Rick Hutcheson <rick.hutcheson@udemy.com>
2015-02-19 21:27:19 -0800

    Adds install scripts for more utils

---
+++ Environment/Variables/PHP.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export PATH=$USER_ENV_UTILS/PHP/latest/bin:$PATH
+
--- Environment/Variables/Python.sh
+++ Environment/Variables/Python.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+export USER_ENV_PYTHON_PY3_VERSION=3.4
 export PATH=$USER_ENV_UTILS/Python/latest/bin:$PATH
+
