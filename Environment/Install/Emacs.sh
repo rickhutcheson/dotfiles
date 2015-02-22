f16ad30339ea3d7e3646d932039a2dd65a4f01e4
Rick Hutcheson <rick@oddlyaccurate.com>
2015-02-22 13:29:12 -0800

    Update Emacs install and config

--- Environment/Install/Emacs.sh
+++ Environment/Install/Emacs.sh
@@ -3,8 +3,18 @@
 ln -s $USER_ENV_CONFIG/Emacs/dot-emacs ~/.emacs
 
 mkdir -p $USER_ENV_UTILS/Emacs
+
+# tern
 cd $USER_ENV_UTILS/Emacs
 git clone https://github.com/marijnh/tern.git tern
 cd tern
 npm install
 ln -s $USER_ENV_UTILS/Emacs/tern ~/.emacs.d/tern
+
+# helm
+# from: https://github.com/emacs-helm/helm
+cd $USER_ENV_UTILS/Emacs
+git clone https://github.com/emacs-helm/helm.git helm
+git clone https://github.com/jwiegley/emacs-async.git async
+cd helm
+make
