5758716db9b28046e05dc24b2d162d435dcc0622
Rick Hutcheson <rick.hutcheson@udemy.com>
2015-04-15 19:40:35 -0700

    magit config and python aliases

--- Environment/Variables/Python.sh
+++ Environment/Variables/Python.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 export USER_ENV_PYTHON_PY3_VERSION=3.4
 export PATH=$USER_ENV_UTILS/Python/latest/bin:$PATH
+alias 2env-default='source $USER_ENV_UTILS/Python/Envs/py2default/bin/activate'
 alias 3env-default='source $USER_ENV_UTILS/Python/Envs/py3default/bin/activate'
