236c02c4984179f1e76f0de207a0b0e4c92594ff
Rick Hutcheson <rick.hutcheson@udemy.com>
2015-05-22 23:20:53 -0700

    Update variables; replace node with io.js

--- Environment/Variables/Python.sh
+++ Environment/Variables/Python.sh
@@ -3,3 +3,12 @@ export USER_ENV_PYTHON_PY3_VERSION=3.4
 export PATH=$USER_ENV_UTILS/Python/latest/bin:$PATH
 alias 2env-default='source $USER_ENV_UTILS/Python/Envs/py2default/bin/activate'
 alias 3env-default='source $USER_ENV_UTILS/Python/Envs/py3default/bin/activate'
+
+# for non-default cases
+pyactivate() {
+    if [ $# -eq 0 ]; then
+        echo "ERROR: Specify virtualenv name."
+    else
+        . $USER_ENV_UTILS/Python/Envs/"$@"/bin/activate
+    fi
+}
--- Environment/Variables/Web.sh
+++ Environment/Variables/Web.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:$USER_ENV_UTILS/php/latest/bin
 export PATH=$PATH:$USER_ENV_UTILS/closure-compiler/latest/bin
 
-export PATH=$PATH:$USER_ENV_UTILS/Node.js/latest/bin
+export PATH=$PATH:$USER_ENV_UTILS/io.js/latest/bin
 export PATH=$PATH:$USER_ENV_UTILS/NPM/node_modules/.bin
 
 # MongoDB
