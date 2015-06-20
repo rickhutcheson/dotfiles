017a2163aa912ed7c9f007c3abcf5b64771cfafb
Rick Hutcheson <rick.hutcheson@udemy.com>
2015-06-20 14:09:52 -0700

    Add nginx and RESOLVE/C++ scripts; update git etc.

--- Environment/Install/Git.sh
+++ Environment/Install/Git.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ln -s $USER_ENV_CONFIG/git/dot-gitconfig ~/.gitconfig
+ln -s $USER_ENV_CONFIG/git/hub-completion.bash ~/.hub-completion.bash
---
+++ Environment/Variables/Resolve.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export RCPP_HOME=~/.rcpp
+export PATH=~/.rcpp/tools:$PATH
