f92e08425d950d27e6e4831f90e0eb95fa66b538
Rick Hutcheson <rick.hutcheson@udemy.com>
2015-06-20 14:23:49 -0700

    Improve nginx configuration.

--- Environment/Install/nginx.sh
+++ Environment/Install/nginx.sh
@@ -5,12 +5,17 @@ mkdir -p $USER_ENV_UTILS/nginx/build
 cd $USER_ENV_UTILS/nginx
 wget http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz -O nginx.tar.gz
 tar xzf nginx.tar.gz -C build --strip-components 1
-mv build nginx-$NGINX_VERSION
+mkdir nginx-$NGINX_VERSION
 rm nginx.tar.gz
 ln -s nginx-$NGINX_VERSION latest
-cd latest
 cd build
-./configure --prefix=$USER_ENV_UTILS/nginx/nginx-$NGINX_VERSION
+./configure \
+    --prefix=$USER_ENV_UTILS/nginx/nginx-$NGINX_VERSION \
+    --with-http_stub_status_module
 make
 make install
-
+cd ..
+mv build nginx-$NGINX_VERSION
+if [ $! = 0 ]; then
+  echo "nginx install Completed."
+fi
