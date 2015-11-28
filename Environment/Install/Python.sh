13ff551e8cbc66be16490d0e1b2536e292f09011
Rick Hutcheson <rick.hutcheson@udemy.com>
2015-11-27 18:11:19 -0800

    Add install script for python source repository.

--- Environment/Install/Python.sh
+++ Environment/Install/Python.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
-
+set -e
 source $USER_ENV_VARS/Python.sh
 
+PY3_VERSION=3.5.0
+
+mkdir -p $USER_ENV_UTILS/Python/python-$PY3_VERSION
+cd $USER_ENV_UTILS/Python
+mkdir -p src
+wget https://www.python.org/ftp/python/$PY3_VERSION/Python-$PY3_VERSION.tar.xz -O python3.tar.xz
+tar xf python3.tar.xz -C src --strip-components 1
+mv src python-$PY3_VERSION
+rm python3.tar.xz
+ln -f -s python-$PY3_VERSION latest
+cd latest
+cd src
+export CPPFLAGS=-I$(brew --prefix openssl)/include;
+export LDFLAGS=-L$(brew --prefix openssl)/lib
+./configure --prefix=$USER_ENV_UTILS/Python/python-$PY3_VERSION
+make
+make install
+
+cd
 easy_install pip
 easy_install-$USER_ENV_PYTHON_PY3_VERSION pip
 pip install virtualenv
@@ -14,4 +33,5 @@ mkdir -p $USER_ENV_UTILS/Python/Envs
 cd $USER_ENV_UTILS/Python/Envs
 
 virtualenv --python=`which python` py2default  # default python2 environment
-virtualenv-$USER_ENV_PYTHON_PY3_VERSION py3default # default python3 environment
+virtualenv-$PY3_VERSION py3default # default python3 environment
+unset PY3_VERSION
