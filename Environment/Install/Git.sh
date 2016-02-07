87cc7fbd059055b215c0d2648c11335b3bbd4ccc
Rick Hutcheson <rick.hutcheson@udemy.com>
2016-02-07 11:16:27 -0800

    Add Git scripts to Install

--- Environment/Install/Git.sh
+++ Environment/Install/Git.sh
@@ -2,3 +2,6 @@
 
 ln -s $USER_ENV_CONFIG/git/dot-gitconfig ~/.gitconfig
 ln -s $USER_ENV_CONFIG/git/hub-completion.bash ~/.hub-completion.bash
+
+mkdir -p $USER_ENV_UTILS/Git/
+cp $USER_ENV_INSTALL/Git/* $USER_ENV_UTILS/Git/
