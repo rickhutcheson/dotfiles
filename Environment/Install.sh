62bee108bf74090ed348b8b21c62dd1f19777f94
Rick Hutcheson <rick@oddlyaccurate.com>
2016-02-07 10:57:43 -0800

    Improve installation script

--- Environment/Install.sh
+++ Environment/Install.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+# Environment Installation 
+# ------------------------
+# Should only be run once, during machine setup. Creates necessary
+# directories and runs all individual setup scripts.
+# 
+# Can be run multiple times; existing installs will be overwritten.
+
 . $USER_ENV_HOME/SetVariables.sh
 
-for SCRIPT in $USER_ENV_INSTALL/*; do
-    if [ -f $SCRIPT ]
+mkdir -p $USER_ENV_SCRIPTS
+mkdir -p $USER_ENV_UTILS
+
+for script in $USER_ENV_INSTALL/*; do
+    if [ -f $script ]
     then
-        $SCRIPT
+        $script
     fi
 done
