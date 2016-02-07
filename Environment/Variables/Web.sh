8aa6c08751cedae9f23b66b6827cf46f658b0fce
Rick Hutcheson <rick.hutcheson@udemy.com>
2016-02-07 10:49:15 -0800

    Remove iojs in favor of Node

--- Environment/Variables/Web.sh
+++ Environment/Variables/Web.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
+export PATH=$USER_ENV_UTILS/Node.js/latest/bin:$PATH
 export PATH=$PATH:$USER_ENV_UTILS/php/latest/bin
 export PATH=$PATH:$USER_ENV_UTILS/closure-compiler/latest/bin
 
-export PATH=$PATH:$USER_ENV_UTILS/io.js/latest/bin
 export PATH=$PATH:$USER_ENV_UTILS/NPM/node_modules/.bin
 export PATH=$PATH:~/node_modules/.bin
 
