1301f7fe8f2e6b8ee282b5c1923475b59e144b5f
Rick Hutcheson <rick.hutcheson@udemy.com>
2016-06-18 10:56:51 -0700

    June 18, 2016

--- Environment/Variables/Web.sh
+++ Environment/Variables/Web.sh
@@ -2,9 +2,12 @@
 export PATH=$USER_ENV_UTILS/Node.js/latest/bin:$PATH
 export PATH=$PATH:$USER_ENV_UTILS/php/latest/bin
 export PATH=$PATH:$USER_ENV_UTILS/closure-compiler/latest/bin
-export PATH=$USER_ENV_UTILS/NPM/node_modules/phantomjs-prebuilt/lib/phantom/bin/:$PATH
 export PATH=$PATH:$USER_ENV_UTILS/NPM/node_modules/.bin
 export PATH=$PATH:~/node_modules/.bin
 
 # MongoDB
 export PATH=$PATH:$USER_ENV_UTILS/mongodb/latest/bin
+
+# PhantomJS
+export PATH=$USER_ENV_UTILS/NPM/node_modules/phantomjs-prebuilt/lib/phantom/bin/:$PATH
+export PHANTOMJS_BIN=`which phantomjs`
