82d1ff7e8068b244ef906871124543582406ba9d
Rick Hutcheson <rick.hutcheson@udemy.com>
2016-10-18 12:52:07 -0700

    Oct. 18, 2016

---
+++ Environment/Install/RedisDesktopManager.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+RDM_VERSION=0.8.0
+
+echo "Make sure you install QT Creator First! (brew install qt5)"
+
+mkdir -p $USER_ENV_UTILS/RedisDesktopManager
+cd $USER_ENV_UTILS/RedisDesktopManager
+mkdir -p rdm-$RDM_VERSION
+ln -f -s rdm-$RDM_VERSION latest
+cd latest
+git clone --recursive https://github.com/uglide/RedisDesktopManager.git -b $RDM_VERSION src && cd ./src
+cd src/  # the repo has its own src/ folder
+
+./configure --prefix=$USER_ENV_UTILS/RedisDesktopManager/rdm-$RDM_VERSION/
+
+export PATH=$PATH:/usr/local/opt/qt5/bin/
+qmake && make && make install
+cd =$USER_ENV_UTILS/RedisDesktopManager/rdm-$RDM_VERSION/bin
+mv qt.conf qt.backup
---
+++ Environment/Variables/RedisDesktopManager.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+export PATH=$PATH:/Users/rickhutcheson/Repositories/dotfiles/Environment/Utilities/RedisDesktopManager/rdm-0.8.0/src/bin/osx/debug/
