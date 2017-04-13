cf91b82869bcc43c0df4cc87f159abe990bad189
Rick Hutcheson <rick.hutcheson@udemy.com>
2017-04-13 10:39:20 -0700

    April 13, 2017

--- Environment/Install/Node.sh
+++ Environment/Install/Node.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-NODE_VERSION=4.4.7
+NODE_VERSION=6.10.1
 mkdir -p $USER_ENV_UTILS/Node.js
 mkdir -p $USER_ENV_UTILS/Node.js/node-$NODE_VERSION
 cd $USER_ENV_UTILS/Node.js
 wget https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$USER_ENV_OS-x64.tar.gz -O node.tar.gz
 tar xzf node.tar.gz -C node-$NODE_VERSION --strip-components 1
 rm node.tar.gz
-ln -s node-$NODE_VERSION latest
+ln -s -f -i node-$NODE_VERSION latest
 
 PKGS="browserify js-beautify docco jshint groc watchify phantomjs-prebuilt"
 
