2c1e89ab9f174fb493060f58a37884a347fcacff
Rick Hutcheson <rick.hutcheson@udemy.com>
2019-03-02 10:30:10 -0800

    Make Python/Yarn installers compatible with OpenBSD

--- Environment/Install/Python.sh
+++ Environment/Install/Python.sh
@@ -6,41 +6,62 @@ export PY3_VERSION=3.5.6
 export PY3_VERSION_SHORT="${PY3_VERSION:0:3}"
 
 echo "Installing $PY3_VERSION"
-rm -rf $USER_ENV_UTILS/Python/python-$PY3_VERSION
-mkdir -p $USER_ENV_UTILS/Python/python-$PY3_VERSION
 cd $USER_ENV_UTILS/Python
-mkdir -p src
+
+# Clean up any previous attempts
+rm -rf $USER_ENV_UTILS/Python/python-$PY3_VERSION
+rm -f python3.tar.xz
+
 echo "Downloading..."
 wget -q https://www.python.org/ftp/python/$PY3_VERSION/Python-$PY3_VERSION.tar.xz -O python3.tar.xz
-echo "Unzipping..."
-tar xf python3.tar.xz -C src --strip-components 1
-mv src python-$PY3_VERSION
+
+echo "Extracting..."
+# If the system has xzcat available, prefer it
+if [ -x "$(command -v xzcat)" ]; then
+    # this lets us get the output directory tar will use
+    dir_name=$(xzcat python3.tar.xz | tar tf - | head -1 | cut -f1 -d"/")
+    xzcat python3.tar.xz | tar xf -
+    mv $dir_name python-$PY3_VERSION
+else
+    # If it doesn't, then it probably has GNU tar
+    tar xf python3.tar.xz -C python-$PY3_VERSION --strip-components 1
+fi
+
 rm python3.tar.xz
 
 echo "Building..."
 cd python-$PY3_VERSION
-cd src
-# sqlite:  IPython needs native sqlite support
-# readline: Proper readline support; otherwise IPython complains about libedit
-export CPPFLAGS="\
+
+# macOS customizations
+if [[ "$USER_ENV_OS" == "darwin" ]]; then
+    # sqlite:  IPython needs native sqlite support
+    # readline: Proper readline support; otherwise IPython complains about libedit
+    export CPPFLAGS="\
        -I$(brew --prefix openssl)/include \
        -I$(brew --prefix sqlite)/include \
        -I$(brew --prefix gettext)/include \
        -I$(brew --prefix zlib)/include \
        -I$(brew --prefix readline)/include \
        -I$(xcrun --show-sdk-path)/usr/include"
-export CFLAGS=$CPPFLAGS
-export LDFLAGS="\
+    export CFLAGS=$CPPFLAGS
+    export LDFLAGS="\
        -L$(brew --prefix openssl)/lib \
        -L$(brew --prefix sqlite)/lib \
        -L$(brew --prefix gettext)/lib \
        -L$(brew --prefix zlib)/lib \
        -L$(brew --prefix readline)/lib"  # Proper readline support; otherwise IPython complains about libedit
-LD_RUN_PATH="$(brew --prefix sqlite)/lib" ./configure --prefix=$USER_ENV_UTILS/Python/python-$PY3_VERSION --with-openssl=$(brew --prefix openssl)
+    LD_RUN_PATH="$(brew --prefix sqlite)/lib" ./configure --prefix=$USER_ENV_UTILS/Python/python-$PY3_VERSION --with-openssl=$(brew --prefix openssl)
+else
+    ./configure --prefix=$USER_ENV_UTILS/Python/python-$PY3_VERSION
+fi
 
 make
 make install
 
+cd $USER_ENV_UTILS/Python
+ln -f -s python-$PY3_VERSION latest
+
+echo "Installing tools..."
 ./bin/easy_install-$PY3_VERSION_SHORT pip
 $USER_ENV_UTILS/Python/$PY3_VERSION/bin/easy_install-$USER_ENV_PYTHON_PY3_VERSION pip
 $USER_ENV_UTILS/Python/$PY3_VERSION/bin/pip install virtualenv
