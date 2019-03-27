3dc2d93712afed0665f43aa6d09794f525772d99
Rick Hutcheson <rick.hutcheson@udemy.com>
2019-03-27 08:01:09 -0700

    Add Memcached installer and fix Python installer (yet again)

---
+++ Environment/Install/Memcached.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+source $USER_ENV_VARS/Memcached.sh
+
+MEMCACHED_VERSION=1.5.12
+
+echo "Installing $MEMCACHED_VERSION"
+
+# Clean up any previous attempts
+rm -rf $USER_ENV_UTILS/Memcached/memcached-$MEMCACHED_VERSION
+rm -f memcached.tar.gz
+
+mkdir -p $USER_ENV_UTILS/Memcached/memcached-$MEMCACHED_VERSION
+cd $USER_ENV_UTILS/Memcached/memcached-$MEMCACHED_VERSION
+
+echo "Downloading..."
+wget -q https://memcached.org/files/memcached-$MEMCACHED_VERSION.tar.gz -O memcached.tar.gz
+
+echo "Extracting..."
+mkdir src
+tar xzf memcached.tar.gz -C src --strip-components 1
+
+rm memcached.tar.gz
+
+echo "Building..."
+cd src
+
+./configure --prefix=$USER_ENV_UTILS/Memcached/memcached-$MEMCACHED_VERSION
+
+make
+make install
+
+echo "Setting up this version as 'latest'"
+cd $USER_ENV_UTILS/Memcached
+rm -f latest
+ln -f -s memcached-$MEMCACHED_VERSION latest
+
+echo "Done!"
--- Environment/Install/Python.sh
+++ Environment/Install/Python.sh
@@ -2,7 +2,7 @@
 set -e
 source $USER_ENV_VARS/Python.sh
 
-export PY3_VERSION=3.5.6
+export PY3_VERSION=3.7.3
 export PY3_VERSION_SHORT="${PY3_VERSION:0:3}"
 
 echo "Installing $PY3_VERSION"
@@ -63,23 +63,14 @@ make install
 cd $USER_ENV_UTILS/Python
 ln -f -s python-$PY3_VERSION latest
 
-echo "Installing tools..."
-./bin/easy_install-$PY3_VERSION_SHORT pip
-$USER_ENV_UTILS/Python/$PY3_VERSION/bin/easy_install-$USER_ENV_PYTHON_PY3_VERSION pip
-$USER_ENV_UTILS/Python/$PY3_VERSION/bin/pip install virtualenv
 
 echo "Setting up this version as 'latest'"
 cd $USER_ENV_UTILS/Python
 unlink latest
 ln -f -s python-$PY3_VERSION latest
 
-
+echo "Creating Envs directory..."
 mkdir -p $USER_ENV_DATA/Python/Envs
-
-# Install a local virtualenv command for use with this system
-cd $USER_ENV_DATA/Python/Envs
-
-#virtualenv --python=`which python` py2default  # default python2 environment
-$USER_ENV_UTILS/Python/latest/bin/virtualenv py3default # default python3 environment
 unset PY3_VERSION
-#$USER_ENV_UTILS/Python/latest/bin/virtualenv py3default # default python3 environment
+
+echo "Done!"
