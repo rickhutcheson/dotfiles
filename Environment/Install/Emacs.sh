29bf03dbe7f6fcd3b58fefcb6b1ddd584148af23
Rick Hutcheson <rick.hutcheson@udemy.com>
2019-12-01 08:00:09 -0800

    Update emacs install script

--- Environment/Install/Emacs.sh
+++ Environment/Install/Emacs.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-EMACS_VERSION=25.2
+EMACS_VERSION=26.3
 mkdir -p $USER_ENV_UTILS/Emacs
 mkdir -p $USER_ENV_UTILS/Emacs/$EMACS_VERSION
 cd $USER_ENV_UTILS/Emacs/$EMACS_VERSION
 wget https://emacsformacosx.com/emacs-builds/Emacs-$EMACS_VERSION-universal.dmg -O Emacs.dmg
 hdiutil attach -mountpoint $USER_ENV_UTILS/Emacs/$EMACS_VERSION/installer Emacs.dmg
-cp -rf $USER_ENV_UTILS/Emacs/$EMACS_VERSION/installer/Emacs.app ~/Applications/
+echo "Copying Emacs.app..."
+cp -Rfv $USER_ENV_UTILS/Emacs/$EMACS_VERSION/installer/Emacs.app ~/Applications/Emacs.app
+echo "Done."
 hdiutil detach $USER_ENV_UTILS/Emacs/$EMACS_VERSION/installer
 cd ..
-unlink $USER_ENV_HOME/.emacs.d
-ln -s  $USER_ENV_DATA/Emacs/ $USER_ENV_HOME/.emacs.d
+unlink ~/.emacs.d
+ln -s  $USER_ENV_DATA/Emacs/ ~/.emacs.d
 exit 0
